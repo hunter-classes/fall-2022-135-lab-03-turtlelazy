ace1c56d973e73af4470ec114bdd4e0b1b163cb3
turtlelazy <ishraqmahid@gmail.com>
2022-09-15 23:50:42 -0400

    main update

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@
 
 int main()
 {
-  std::cout << get_east_storage("05/20/2018") << std::endl;
-  std::cout << get_min_east() << std::endl;
-  std::cout << get_max_east() << std::endl;
+  std::cout << "05/20/2018 East Storage: " << get_east_storage("05/20/2018") << std::endl;
+  std::cout << "Minimum East Basin Storage: " << get_min_east() << std::endl;
+  std::cout << "Maximum East Basin Storage: " << get_max_east() << std::endl;
 
-  std::cout << get_east_storage("09/13/2018") << std::endl;
+  std::cout << "09/13/2018 East Storage: " << get_east_storage("09/13/2018") << std::endl;
   std::cout << get_west_storage("09/13/2018") << std::endl;
 
-  std::cout << compare_basins("09/13/2018") << std::endl;
-  std::cout << compare_basins("09/14/2018") << std::endl;
-  std::cout << compare_basins("09/15/2018") << std::endl;
+  std::cout << "Compare Basin 09/13/2018: " << compare_basins("09/13/2018") << std::endl;
+  std::cout << "Compare Basin 09/14/2018: " << compare_basins("09/14/2018") << std::endl;
+  std::cout << "Compare Basin 09/15/2018: " << compare_basins("09/15/2018") << std::endl;
 
   reverse_oreder("05/29/2018", "06/02/2018");
   return 0;
